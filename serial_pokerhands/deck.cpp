bf04030600f02e1f6f5d7d3614f5ac70862095c7
Jordon de Hoog <jordon.dehoog@gmail.com>
2014-09-23 16:27:41 -0400

    fixed random number seed for the random shuffle

--- serial_pokerhands/deck.cpp
+++ serial_pokerhands/deck.cpp
@@ -1,6 +1,17 @@
 #include "deck.h"
 
 Deck::Deck() : size_(0), decks_(0) {
+    Deck::stats_[0].Type = "No Pair";
+    Deck::stats_[1].Type = "One Pair";
+    Deck::stats_[2].Type = "Two Pair";
+    Deck::stats_[3].Type = "Three of a Kind";
+    Deck::stats_[4].Type = "Straight";
+    Deck::stats_[5].Type = "Flush";
+    Deck::stats_[6].Type = "Full House";
+    Deck::stats_[7].Type = "Four of a Kind";
+    Deck::stats_[8].Type = "Straight Flush";
+    Deck::stats_[9].Type = "Royal Flush";
+
     Deck::create();
 }
 
@@ -23,6 +34,7 @@ void Deck::create() {
 }
 
 void Deck::shuffle() {
+    std::srand(unsigned(std::time(0)));
     std::random_shuffle(&cards_[0], &cards_[MAX_DECK_SIZE - 1]);
 }
 
