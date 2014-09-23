e8c9907d74a07674729be0c249a8cedd237c9b32
Jordon de Hoog <jordon.dehoog@gmail.com>
2014-09-23 13:38:33 -0400

    misc changes

--- serial_pokerhands/serial_main.cpp
+++ serial_pokerhands/serial_main.cpp
@@ -1,14 +1,6 @@
 #include "serial_main.h"
 
 int main() {
-    /*std::cout << "test" << std::endl;
-    Deck test;
-
-    for (int i = 0; i < test.getDeck().size(); ++i) {
-        Card c = test.draw();
-        std::cout << "Suit: " << c.getSuit() << " Rank: " << c.getRank() << " SIZE: " << test.getDeckSize() << std::endl;
-    }*/
-
     //DEBUG
     Deck d;
     int hands = 0;
