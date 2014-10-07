486f92d4dd5b60fc1c6d4296bf8f9e12a1633e6d
Jonathan Decher <jondecher@gmail.com>
2014-10-06 23:23:34 -0400

    test

---
+++ hooger-pokerhands-48b807d6fb1f/hooger-pokerhands-48b807d6fb1f/serial_pokerhands/card.cpp
@@ -0,0 +1,50 @@
+#include "card.h"
+
+const int Card::SUITS[4] = { 0, 1, 2, 3 };
+const int Card::RANKS[13] = { 1,2,3,4,5,6,7,8,9,10,11,12,13 };
+
+Card::Card() : suit_(-1), rank_(-1) {}
+
+Card::Card(int s, int r) : suit_(s), rank_(r) {}
+
+int Card::suit() {
+    return suit_;
+}
+
+int Card::rank() {
+    return rank_;
+}
+
+std::string Card::suit(int s) {
+    switch (s)
+    {
+    case 0:
+        return "D"; //Diamond
+    case 1:
+        return "H"; //Heart
+    case 2:
+        return "C"; //Club
+    case 3:
+        return "S"; //Spade
+    default:
+        return "U"; //unknown
+    }
+}
+
+std::string Card::rank(int r) {
+    switch (r)
+    {
+    case 1:
+        return "a";
+    case 11:
+        return "j";
+    case 12:
+        return "q";
+    case 13:
+        return "k";
+    default:
+        std::stringstream ss;
+        ss << r;
+        return ss.str(); //non monkey
+    }
+}
